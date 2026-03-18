a090b440ac5fd78b26e4e0b5e40885d33562ed2b
Kaj Kowalski <info@kajkowalski.nl>
2026-03-18 17:11:51 +0100

    feat(queries): improve highlights, add tags/locals
    - highlights: dn:: keywords, DN values as @label via
      inner-node capture, modify ops as @keyword.operator
    - injections: case-insensitive regex with (?i) flag
    - tags: DN-based code navigation for entries
    - locals: scope/definition tracking for attributes

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,5 +1,7 @@
+; Inject ldap-schema into OpenLDAP OLC schema definition attributes.
+; Case-insensitive match because LDAP attribute names are case-insensitive.
 (attrval_spec
   (attribute_name) @_attr
   (value) @injection.content
-  (#match? @_attr "^olc(AttributeTypes|ObjectClasses|LdapSyntaxes|DitContentRules|MatchingRules|MatchingRuleUse|NameForms)$")
+  (#match? @_attr "(?i)^olc(AttributeTypes|ObjectClasses|LdapSyntaxes|DitContentRules|MatchingRules|MatchingRuleUse|NameForms)(;[a-zA-Z0-9-]+)*$")
   (#set! injection.language "ldap-schema"))
